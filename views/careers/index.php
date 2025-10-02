f3eca22edf88c7ae38f69606875eb6c916e7b55c
Idongesit Ataide <idongataide@gmail.com>
2025-10-02 14:27:50 +0100

    update

--- views/careers/index.php
+++ views/careers/index.php
@@ -82,6 +82,36 @@
 </section>
 
 
+<section class="pt-4 overflow-hidden" id="bemore">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="border-radius-6px h-500px md-h-450px sm-h-350px d-flex align-items-end justify-content-center overflow-hidden cover-background skrollr-zoom  xs-pb-12" >
+                    <video autoplay muted loop playsinline>
+                        <source src="<?php echo base_url(); ?>/assets/img/careers/bemore.mp4" type="video/mp4">
+                    </video>
+                    <div class="opacity-medium bg-gradient-dark-transparent"></div>
+                    <div class="row justify-content-center position-absolute">
+                        <div class="col-11 col-md-7 pb-8 position-relative z-index-1 text-center text-lg-start md-mb-35px sm-mb-25px">
+                            <h4 class="alt-font text-white mb-0 fw-300 fancy-text-style-4">Click here to 
+                                <span class="fw-600" data-fancy-text='{ "effect": "rotate", "string": ["Be more"] }'></span>
+                            </h4> 
+                        </div>
+                        <div class="col-xl-5 col-lg-3 position-relative z-index-1 text-center animation-zoom">
+                            <a href="https://www.youtube.com/watch?v=lop6t2QSZLs" class="position-relative d-inline-block text-center border border-3 border-color-transparent-white-very-light rounded-circle video-icon-box video-icon-large popup-youtube">
+                                <span>
+                                    <span class="video-icon">
+                                        <i class="bi bi-play-fill text-white"></i>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <section class="overlap-height bg-very-light-gray">
         <div class="container overlap-gap-section">
             <div class="row">
