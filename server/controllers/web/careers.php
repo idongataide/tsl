0cd873f2575d43e747b3bcc67b9d68bffe9687f3
Idongesit Ataide <idongataide@gmail.com>
2026-01-15 19:26:02 +0100

    updates

--- server/controllers/web/careers.php
+++ server/controllers/web/careers.php
@@ -84,31 +84,41 @@ class careers extends ServerController
     public function experienced_hires()
     {
         $curl = curl_init();
-            curl_setopt_array($curl, [
+        curl_setopt_array($curl, [
             CURLOPT_URL => 'https://tslapi.ufainiibom.com/api/jobs',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => 'GET',
             CURLOPT_HTTPHEADER => [
                 'Content-Type: application/json',
                 'Accept: application/json',
-                // 'Cache-Control: no-cache',
             ],
-    
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false,
         ]);
-    
+
         $response = curl_exec($curl);
         curl_close($curl);
+
         $decodedResponse = json_decode($response, true);
-        
-        $data['jobs'] = $decodedResponse['data'] ?? [];
+
+        $jobs = [];
+        if (isset($decodedResponse['data']) && is_array($decodedResponse['data'])) {
+            foreach ($decodedResponse['data'] as $job) {
+                if (isset($job['isPublished']) && $job['isPublished'] == '1') {
+                    $jobs[] = $job;
+                }
+            }
+        }
+
+        $data['jobs'] = $jobs;
         $data['page_title'] = 'Experience Hires';
         $data['type'] = 'service';
         $data['service'] = 'Experience Hires';
         $data['menu_active'] = 'experienced_hires';
-        $this->loadView('careers/experienced_hires',  @$data);
+
+        $this->loadView('careers/experienced_hires', $data);
     }
+
 
 
     
