56f2f855014e4f0fe942b12eef226cac7e62e144
Idongesit Ataide <idongataide@gmail.com>
2026-04-17 18:49:26 +0100

    first commit

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -77,11 +77,11 @@
        <div id="cookies-model" class="cookie-message bg-dark-gray border-radius-8px"> 
             <div class="cookie-description fs-14 text-white mb-20px lh-22">We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking "Allow cookies" you consent to our use of cookies. </div>   
             <div class="cookie-btn">
-                <!-- <a href="#" class="btn btn-transparent-white border-1 border-color-transparent-white-light btn-very-small btn-switch-text btn-rounded w-100 mb-15px" aria-label="btn">
+                <a href="<?php echo base_url(); ?>/privacy" class="btn btn-transparent-white border-1 border-color-transparent-white-light btn-very-small btn-switch-text btn-rounded w-100 mb-15px" aria-label="btn">
                     <span>
                         <span class="btn-double-text" data-text="Cookie policy">Cookie policy</span> 
                     </span>
-                </a>  -->
+                </a> 
                 <a href="#" class="btn btn-white btn-very-small btn-switch-text btn-box-shadow accept_cookies_btn btn-rounded w-100" data-accept-btn="" aria-label="text">
                     <span>
                         <span class="btn-double-text" data-text="Allow cookies">Allow cookies</span> 
@@ -212,7 +212,7 @@
                             });
                         }
 
-                });
-            </script>
+    });
+</script>
     </body>
 </html>
